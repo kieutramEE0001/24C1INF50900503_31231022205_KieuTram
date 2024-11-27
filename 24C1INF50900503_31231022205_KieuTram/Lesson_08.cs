ffce82d66654b3213e86ab77e65f20fb87041ec3
Kieu Tram <Kieu Tram@LAPTOP-1RMFHO1A>
2024-11-27 10:51:39 +0700

    update

--- 24C1INF50900503_31231022205_KieuTram/Lesson_08.cs
+++ 24C1INF50900503_31231022205_KieuTram/Lesson_08.cs
@@ -14,25 +14,28 @@ namespace _24C1INF50900503_31231022205_KieuTram
             //string s1 = "hello";
             //s1 = "world";
             //string? s2 = null; //Không cần kiểm tra
-            string s = "Enumerates    and transforms a    sequence, and procedures an immutable" + 
+            string s = "Enumerates    and transforms a    sequence and procedures an immutable" + 
                 "sorted dictionary of its contents by using the   specified key and value comparers.";
             int len = CountWords(s);
             Console.WriteLine(len);
         }
         static int CountWords(string s)
         {
-            int count = 1;
+            int count = 0;
             //Bỏ khoảng trắng thừa có trong chuỗi
             s = s.Trim(); //bỏ khoảng trắng ở đầu và cuối chuỗi
             while (s.IndexOf("  ") != -1)
             {
                 s = s.Replace("  ", " ");
-            } 
+            }
+            string[] words = s.Split(' ');
+            count = words.Length;
+            return count +1;
             //đếm
-            foreach (char c in s) 
-                if (c == ' ')
-                    count++;
-            return count;
+            //foreach (char c in s) 
+            //    if (c == ' ')
+            //        count++;
+            //return count +1;
         }
     }
 }
